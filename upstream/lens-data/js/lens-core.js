7ea49b723f07a159b52673d4af5b87a4cedf7877
Dustin Falgout <dustin@falgout.us>
2015-11-13 04:20:31 -0600

    started working on using qtwebchannel

--- upstream/lens-data/js/lens-core.js
+++ upstream/lens-data/js/lens-core.js
@@ -1,51 +1,64 @@
 window.lens = {
-  "__cb":{},
-  "__cb_once":{},
-  "__emit":function(data) {
-    if (data.length > 0) {
-      var name = data[0];
-      var args = data.slice(1);
+	"__cb": {},
+	"__cb_once": {},
+	"__emit": function (data) {
+		if (data.length > 0) {
+			var name = data[0];
+			var args = data.slice(1);
 
-      s = window.lens.__cb[name] || [];
+			s = window.lens.__cb[name] || [];
 
-      gs = window.lens.__cb['__*'] || [];
+			gs = window.lens.__cb['__*'] || [];
 
-      so = window.lens.__cb_once[name] || [];
-      delete window.lens.__cb_once[name];
+			so = window.lens.__cb_once[name] || [];
+			delete window.lens.__cb_once[name];
 
-      s.forEach(function(cb) { cb.apply(undefined, args); });
-      so.forEach(function(cb) { cb.apply(undefined, args); });
-      gs.forEach(function(cb) { cb.apply(undefined, data); });
-    }
-  },
-  "emit":function() {
-    var _args = Array.prototype.slice.call(arguments);
+			s.forEach(function (cb) {
+				cb.apply(undefined, args);
+			});
+			so.forEach(function (cb) {
+				cb.apply(undefined, args);
+			});
+			gs.forEach(function (cb) {
+				cb.apply(undefined, data);
+			});
+		}
+	},
+	"emit": function () {
+		var _args = Array.prototype.slice.call(arguments);
 
-    if (_args.length > 0) {
-      var c = {name: _args.shift(), args: _args};
+		if (_args.length > 0) {
+			var msg = {name: _args.shift(), args: _args};
 
-      /* update document title */
-      var t = document.title;
-      document.title = '_BR::' + JSON.stringify(c);
-      document.title = t;
-    }
-  },
-  "has_subscribers":function(name) {
-    s = window.lens.__cb[name] || [];
-    so = window.lens.__cb_once[name] || [];
+			try {
+				/* If we are using QtWebChannel */
+				msg['name'](_args);
+			}
+			finally {
+				/* update document title */
+				var t = document.title;
+				document.title = '_BR::' + JSON.stringify(msg);
+				document.title = t;
+			}
 
-    return s.length + so.length;
-  },
-  "on":function(name, cb) {
-    window.lens.__cb[name] = window.lens.__cb[name] || [];
-    window.lens.__cb[name].push(cb);
-  },
-  "on_any":function(cb) {
-    window.lens.__cb['__*'] = window.lens.__cb['__*'] || [];
-    window.lens.__cb['__*'].push(cb);
-  },
-  "once":function(name, cb) {
-    window.lens.__cb_once[name] = window.lens.__cb[name] || [];
-    window.lens.__cb_once[name].push(cb);
-  }
+		}
+	},
+	"has_subscribers": function (name) {
+		s = window.lens.__cb[name] || [];
+		so = window.lens.__cb_once[name] || [];
+
+		return s.length + so.length;
+	},
+	"on": function (name, cb) {
+		window.lens.__cb[name] = window.lens.__cb[name] || [];
+		window.lens.__cb[name].push(cb);
+	},
+	"on_any": function (cb) {
+		window.lens.__cb['__*'] = window.lens.__cb['__*'] || [];
+		window.lens.__cb['__*'].push(cb);
+	},
+	"once": function (name, cb) {
+		window.lens.__cb_once[name] = window.lens.__cb[name] || [];
+		window.lens.__cb_once[name].push(cb);
+	}
 };
